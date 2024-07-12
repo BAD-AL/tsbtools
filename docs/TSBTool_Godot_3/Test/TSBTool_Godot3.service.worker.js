aebc9e4ee32436ce50f15c83442c8b2f46d32520
BAD-AL <christopherbadal@yahoo.com>
2024-07-12 14:53:30 -0600

    more testing

--- docs/TSBTool_Godot_3/Test/TSBTool_Godot3.service.worker.js
+++ docs/TSBTool_Godot_3/Test/TSBTool_Godot3.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1720816514|60848730822";
+const CACHE_VERSION = "1720817537|61871510763";
 const CACHE_PREFIX = "TSBTool_Godot_3-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "TSBTool_Godot3.offline.html";
